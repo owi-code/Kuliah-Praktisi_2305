90ebcbbbba27e154b49eec20a3a3d39905ba96bf
owi-code <eniftmwtii@gmail.com>
2026-05-23 10:20:01 +0700

    app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ st.set_page_config(
   page_icon="🧊"
   layout="centered"
   initial_sidebar_state="expanded",
-    menu_items={
 )
 
 st.title("📊 Dashboard")
 st.header("Laporan Bulanan")
 st.subheader("📈 Monthly Expenses")
 st.caption("Made with ❤️ using Streamlit")
+st.write("Hello, *World!* :sunglasses:")
