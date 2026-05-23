7252a00da2e7681f76526a0e46264d984090aa29
owi-code <eniftmwtii@gmail.com>
2026-05-23 10:16:22 +0700

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
+
 st.set_page_config(
   page_title="Kuliah Praktisi 2305",
   page_icon="🧊"
   layout="centered"
+  initial_sidebar_state="expanded",
+    menu_items={
 )
 
 st.title("📊 Dashboard")
